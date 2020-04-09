85e7afc49736752ba25d5c1f5e91bf50f27ab204
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-04-09 18:57:25 +1000

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -82,6 +82,7 @@ copyright = u'2016, Vighnesh Birodkar'
 # built documents.
 #
 # The short X.Y version.
+sys.path.append("../")
 from mne_nirs import __version__
 version = __version__
 # The full version, including alpha/beta/rc tags.
