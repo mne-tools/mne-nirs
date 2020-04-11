8e2a396da0f36c276b9d2d513def7afc2e79348b
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-04-11 15:00:35 +1000

    Add HRF code

--- mne_nirs/_experimental_design.py
+++ mne_nirs/_experimental_design.py
@@ -12,3 +12,20 @@ def foo():
     """
     print(2 * 2)
     return 'foo'
+
+
+def create_boxcar(raw, event_id=None, stim_dur=5):
+    from scipy import signal
+    from bids.analysis import hrf
+    bc = signal.boxcar(round(raw.info['sfreq']*stim_dur))
+    events, ids = mne.events_from_annotations(raw, event_id=event_id)
+    s = np.zeros((len(raw.times), len(ids)))
+    h0 = hrf._gamma_difference_hrf(raw.info['sfreq'], time_length = 32, delay=4)
+    for idx, id in enumerate(ids):
+        id_idx = [e[2] == idx+1 for e in events]
+        id_evt = events[id_idx]
+        event_samples = [e[0] for e in id_evt]
+        s[event_samples, idx] = 1.
+        s[:, idx] = np.convolve(s[:, idx], bc)[:len(raw.times)]
+        s[:, idx] = np.convolve(s[:, idx], h0)[:len(raw.times)]
+    return s
