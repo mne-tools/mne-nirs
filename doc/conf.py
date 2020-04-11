fe02b4fc5427199c36468ef2f7f9edb2269d14e2
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-04-11 13:22:29 +1000

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -75,7 +75,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'mne-nirs'
-copyright = u'2016, Vighnesh Birodkar'
+copyright = u'2016, Robert Luke'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -233,7 +233,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'project-template.tex', u'project-template Documentation',
-   u'Vighnesh Birodkar', 'manual'),
+   u'Robert Luke', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -263,7 +263,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'project-template', u'project-template Documentation',
-     [u'Vighnesh Birodkar'], 1)
+     [u'Robert Luke'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -277,7 +277,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'project-template', u'project-template Documentation',
-   u'Vighnesh Birodkar', 'project-template', 'One line description of project.',
+   u'Robert Luke', 'project-template', 'One line description of project.',
    'Miscellaneous'),
 ]
 
