9cf74018eb14eecb22f33017fd0a0629db7e253f
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-04-12 12:39:56 +1000

    Make it clear which functions are temporary while I am setting up this repo and the auto documentation

--- mne_nirs/__init__.py
+++ mne_nirs/__init__.py
@@ -1,4 +1,4 @@
 from ._version import __version__
-from ._experimental_design import foo, create_boxcar, create_hrf
+from ._experimental_design import create_boxcar, create_hrf
 
 __all__ = ['__version__', 'foo']
