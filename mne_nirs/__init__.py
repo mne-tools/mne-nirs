866506e88882038e0447e8c0583aa38414402c57
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-04-13 15:57:47 +1000

    Update __init__.py

--- mne_nirs/__init__.py
+++ mne_nirs/__init__.py
@@ -3,4 +3,4 @@ from ._version import __version__
 from . import signal_enhancement
 from . import experimental_design
 
-__all__ = ['__version__', 'create_boxcar', 'create_hrf']
+__all__ = ['__version__']
