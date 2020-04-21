920d94ac1bf4bbd99c80c3e69e3de03dde589f3e
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-04-21 21:22:45 +1000

    Update test_plot_nirs_source_detector.py

--- mne_nirs/visualisation/tests/test_plot_nirs_source_detector.py
+++ mne_nirs/visualisation/tests/test_plot_nirs_source_detector.py
@@ -5,12 +5,12 @@
 import mne
 import mne_nirs
 import numpy as np
-from mne.utils import (requires_pysurfer, traits_test)		
+from mne.utils import (requires_pysurfer, traits_test)
 
 mne.viz.set_3d_backend('pyvista')
 
- 
-@requires_pysurfer		
+
+@requires_pysurfer
 @traits_test
 def test_plot_nirs_source_detector():
     data_path = mne.datasets.testing.data_path()
