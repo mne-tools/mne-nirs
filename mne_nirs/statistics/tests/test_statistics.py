5b9806295e9f60d85b766ec7ab852bfbd06e8584
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-05-03 13:48:31 +1000

    Move GLM tests to simualation data so result can be evaluated

--- mne_nirs/statistics/tests/test_statistics.py
+++ mne_nirs/statistics/tests/test_statistics.py
@@ -3,28 +3,26 @@
 # License: BSD (3-clause)
 
 
-import mne
 from mne_nirs.experimental_design import create_first_level_design_matrix
 from mne_nirs.statistics import run_GLM
-from mne_nirs.experimental_design.tests.test_experimental_design import \
-    _load_dataset
+from mne_nirs.simulation import simulate_nirs_raw
 
 
 def test_run_GLM():
-    raw_intensity = _load_dataset()
-    raw_intensity.crop(450, 600)  # Keep the test fast
-    design_matrix = create_first_level_design_matrix(raw_intensity,
+    raw = simulate_nirs_raw(sig_dur=200, stim_dur=5.)
+    design_matrix = create_first_level_design_matrix(raw, stim_dur=5.,
                                                      drift_order=1,
                                                      drift_model='polynomial')
-    raw_od = mne.preprocessing.nirs.optical_density(raw_intensity)
-    raw_haemo = mne.preprocessing.nirs.beer_lambert_law(raw_od)
-    labels, glm_estimates = run_GLM(raw_haemo, design_matrix)
+    labels, glm_estimates = run_GLM(raw, design_matrix)
 
-    assert len(labels) == len(raw_haemo.ch_names)
+    assert len(labels) == len(raw.ch_names)
 
     # the estimates are nested. so cycle through to check correct number
     # are generated
     num = 0
     for est in glm_estimates:
         num += glm_estimates[est].theta.shape[1]
-    assert num == len(raw_haemo.ch_names)
+    assert num == len(raw.ch_names)
+
+    # Check the estimate is correct within 10% error
+    assert abs(glm_estimates[labels[0]].theta[0]- 1.e-6) < 0.1e-6
