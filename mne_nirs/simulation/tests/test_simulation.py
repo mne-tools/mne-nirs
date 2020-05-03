eea651f8de5f57c87861ff56f35d97d25d3a0a3f
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-05-03 13:34:12 +1000

    Add tests for simulation

--- mne_nirs/simulation/tests/test_simulation.py
+++ mne_nirs/simulation/tests/test_simulation.py
@@ -2,9 +2,20 @@
 #
 # License: BSD (3-clause)
 
-import mne_nirs
+from mne_nirs.simulation import simulate_nirs_raw
+import numpy as np
 
 
 def test_simulate_NIRS():
-    raw = mne_nirs.simulation.simulate_nirs_raw()
+
+    raw = simulate_nirs_raw(sfreq=3., amplitude=1.,
+                            sig_dur=300., stim_dur=5.,
+                            isi_min=15., isi_max=45.)
     assert 'hbo' in raw
+    assert raw.info['sfreq'] == 3.
+    assert raw.get_data().shape == (1, 900)
+    assert np.max(raw.get_data()) < 1.2 * 1.e-6
+    assert raw.annotations.description[0] == 'A'
+    assert raw.annotations.duration[0] == 5
+    assert np.min(np.diff(raw.annotations.onset)) > 15. + 5.
+    assert np.max(np.diff(raw.annotations.onset)) < 45. + 5.
