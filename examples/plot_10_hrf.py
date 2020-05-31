134aa93a33cea2243fb0045d3e9e6db2a20cb225
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-05-31 21:41:08 +1000

    Add tidy data export (#97)

--- examples/plot_10_hrf.py
+++ examples/plot_10_hrf.py
@@ -35,12 +35,14 @@ import os
 import matplotlib.pyplot as plt
 import mne
 import mne_nirs
+import numpy as np
 
 from mne_nirs.experimental_design import make_first_level_design_matrix
 from mne_nirs.statistics import run_GLM
 from mne_nirs.visualisation import plot_GLM_topo
 
 from nilearn.reporting import plot_design_matrix
+from mne_nirs.utils._io import _GLM_to_tidy_long, _tidy_long_to_wide
 
 
 ###############################################################################
@@ -83,6 +85,8 @@ raw_intensity.annotations.crop(35, 2967)
 
 raw_od = mne.preprocessing.nirs.optical_density(raw_intensity)
 raw_haemo = mne.preprocessing.nirs.beer_lambert_law(raw_od)
+
+short_chans = mne_nirs.utils.get_short_channels(raw_haemo)
 raw_haemo = mne_nirs.utils.get_long_channels(raw_haemo)
 
 
@@ -96,6 +100,7 @@ events, _ = mne.events_from_annotations(raw_haemo)
 event_dict = {'Control': 1, 'Tapping/Left': 2, 'Tapping/Right': 3}
 mne.viz.plot_events(events, event_id=event_dict,
                     sfreq=raw_haemo.info['sfreq'])
+
 
 ###############################################################################
 #
@@ -115,12 +120,25 @@ plt.xlabel("Time (s)")
 # This analysis specifies the experiment using a design matrix which
 # is created and plotted below.
 # In this example we use the standard SPM haemodynamic response function and
-# include a first order polynomial drift.
+# include a third order polynomial drift.
 
 design_matrix = make_first_level_design_matrix(raw_intensity,
                                                hrf_model='spm', stim_dur=5.0,
-                                               drift_order=1,
+                                               drift_order=3,
                                                drift_model='polynomial')
+
+
+###############################################################################
+#
+# Next we add the mean of the short channels to the design matrix
+# as these channels contain systemic but not neural responses.
+
+design_matrix["ShortHbO"] = np.mean(short_chans.copy().pick(
+                                    picks="hbo").get_data(), axis=0)
+
+design_matrix["ShortHbR"] = np.mean(short_chans.copy().pick(
+                                    picks="hbr").get_data(), axis=0)
+
 
 ###############################################################################
 #
@@ -132,14 +150,17 @@ fig = plot_design_matrix(design_matrix, ax=ax1)
 
 
 ###############################################################################
+# Examine expected response
+# -------------------------
 #
-# And we can also look at a single experimental condition.
+# We can also look at a single experimental condition with a boxcar
+# function representing the stimulus, and the expected neural response.
 
 s = mne_nirs.experimental_design.create_boxcar(raw_intensity)
 plt.plot(raw_intensity.times, s[:, 1])
 plt.plot(design_matrix['Tapping/Left'])
 plt.xlim(180, 300)
-plt.legend(["Stimulus", "Expected HRF"])
+plt.legend(["Stimulus", "Expected Response"])
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 
@@ -152,8 +173,8 @@ plt.ylabel("Amplitude")
 # First we analyse just the first two channels which correspond HbO and HbR
 # of a single source detector pair.
 
-labels, glm_estimates = run_GLM(raw_haemo.copy().pick(picks=range(2)),
-                                design_matrix)
+labels, glm_est = run_GLM(raw_haemo.copy().pick(picks=range(2)), design_matrix)
+
 
 ###############################################################################
 #
@@ -164,10 +185,8 @@ labels, glm_estimates = run_GLM(raw_haemo.copy().pick(picks=range(2)),
 # right hand is larger than the left. And the values are similar to what
 # is seen in the epoching tutorial.
 
-plt.scatter(design_matrix.columns[:3],
-            glm_estimates[labels[0]].theta[:3] * 1e6)
-plt.scatter(design_matrix.columns[:3],
-            glm_estimates[labels[1]].theta[:3] * 1e6)
+plt.scatter(design_matrix.columns[:3], glm_est[labels[0]].theta[:3] * 1e6)
+plt.scatter(design_matrix.columns[:3], glm_est[labels[1]].theta[:3] * 1e6)
 plt.xlabel("Experiment Condition")
 plt.ylabel("Haemoglobin (μM)")
 plt.legend(["Oxyhaemoglobin", "Deoxyhaemoglobin"])
@@ -176,7 +195,7 @@ plt.show()
 
 
 ###############################################################################
-# View GLM resufvlts for all sensors
+# View GLM results for all sensors
 # --------------------------------
 #
 # Lastly we run the GLM analysis on all sensors and plot the result on a
@@ -185,6 +204,46 @@ plt.show()
 # contralateral to the tapping side. Also note that HbR tends to be the
 # negative sof HbO as expected.
 
-labels, glm_estimates = run_GLM(raw_haemo, design_matrix)
-plot_GLM_topo(raw_haemo, labels, glm_estimates, design_matrix,
+labels, glm_est = run_GLM(raw_haemo, design_matrix)
+plot_GLM_topo(raw_haemo, labels, glm_est, design_matrix,
               requested_conditions=['Tapping/Left', 'Tapping/Right'])
+
+
+###############################################################################
+# Export Results
+# ---------------
+#
+# .. warning::
+#       The functions used in this section are in development, and are highly
+#       likely to change. These functions are marked with an underscore (_)
+#       at the start of their name to indicate they are not public functions
+#       and have no promise they will be here next week.
+
+
+df = _GLM_to_tidy_long(raw_haemo, labels, glm_est, design_matrix)
+df = _tidy_long_to_wide(df)
+
+
+###############################################################################
+# Determine true and false positive rates
+# ---------------------------------------
+#
+# We can query the exported data frames to determine the true and false
+# positive rates. Note: optodes cover a greater region than just the
+# motor cortex, so we dont expect 100% of channels to detect responses to
+# the tapping, but we do expect 5% or less for the false positive rate.
+
+fp_rate = np.sum(df.query("condition in ['Control']")["Significant"]) / \
+    len((df.query("condition in ['Control']")["Significant"]))
+
+tp_left_rate = np.sum(
+    df.query("condition in ['Tapping/Left']")["Significant"]) / \
+    len((df.query("condition in ['Tapping/Left']")["Significant"]))
+
+tp_right_rate = np.sum(
+    df.query("condition in ['Tapping/Right']")["Significant"]) / \
+    len((df.query("condition in ['Tapping/Right']")["Significant"]))
+
+print("False positive rate:", 100. * fp_rate, "(%)")
+print("True positive rate (left):", 100. * tp_left_rate, "(%)")
+print("True positive rate (right):", 100. * tp_right_rate, "(%)")
---
+++ mne_nirs/utils/_io.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from scipy import stats
+import numpy as np
+import re
+
+
+def _GLM_to_tidy_long(data, labels, glm_estimates, design_matrix):
+
+    theta_estimates = np.zeros((len(labels), len(design_matrix.columns)))
+    t_estimates = np.zeros((len(labels), len(design_matrix.columns)))
+    df_estimates = np.zeros((len(labels), len(design_matrix.columns)))
+    p_estimates = np.zeros((len(labels), len(design_matrix.columns)))
+    mse_estimates = np.zeros((len(labels), len(design_matrix.columns)))
+
+    for idx, lab in enumerate(labels):
+        matching_labels = np.where(([lab == l for l in labels]))
+        matching_idx = np.where([idx == ml for ml in matching_labels])[1]
+        theta_estimates[idx, :] = glm_estimates[lab].theta[:, matching_idx].T
+        df_estimates[idx, :] = glm_estimates[lab].df_model
+        mse_estimates[idx, :] = glm_estimates[lab].MSE[0]
+        for cond_idx, cond in enumerate(design_matrix.columns):
+            t_estimates[idx, cond_idx] = glm_estimates[lab].t(
+                column=cond_idx)[matching_idx]
+            p_estimates[idx, cond_idx] = 1 - stats.t.cdf(
+                t_estimates[idx, cond_idx], df=df_estimates[idx, cond_idx])
+
+    df = pd.DataFrame()
+
+    for ch_idx, ch in enumerate(data.ch_names):
+        for cond_idx, cond in enumerate(design_matrix.columns):
+            df = df.append({'ch_name': ch, 'condition': cond,
+                            'variable': "theta",
+                            'value': theta_estimates[ch_idx][cond_idx]},
+                           ignore_index=True)
+            df = df.append({'ch_name': ch, 'condition': cond,
+                            'variable': "t",
+                            'value': t_estimates[ch_idx][cond_idx]},
+                           ignore_index=True)
+            df = df.append({'ch_name': ch, 'condition': cond,
+                            'variable': "df",
+                            'value': df_estimates[ch_idx][cond_idx]},
+                           ignore_index=True)
+            df = df.append({'ch_name': ch, 'condition': cond,
+                            'variable': "p",
+                            'value': p_estimates[ch_idx][cond_idx]},
+                           ignore_index=True)
+            df = df.append({'ch_name': ch, 'condition': cond,
+                            'variable': "mse",
+                            'value': mse_estimates[ch_idx][cond_idx]},
+                           ignore_index=True)
+
+    return df
+
+
+def _tidy_long_to_wide(d, expand_output=True):
+    d = d.set_index(['ch_name', 'condition'])
+    d = d.pivot_table(columns='variable', values='value',
+                      index=['ch_name', 'condition'])
+    d.reset_index(inplace=True)
+
+    if expand_output:
+        d["Source"] = [re.search(r'S(\d+)_D(\d+) (\w+)', ch).group(1)
+                       for ch in d["ch_name"]]
+        d["Detector"] = [re.search(r'S(\d+)_D(\d+) (\w+)', ch).group(2)
+                         for ch in d["ch_name"]]
+        d["Chroma"] = [re.search(r'S(\d+)_D(\d+) (\w+)', ch).group(3)
+                       for ch in d["ch_name"]]
+        d["Significant"] = d["p"] < 0.05
+
+    return d
