340ac8bb96dd0a830bb99f70863dd2878316fc85
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-05-31 18:06:01 +1000

    Flake fixes

--- mne_nirs/utils/_io.py
+++ mne_nirs/utils/_io.py
@@ -2,6 +2,7 @@ import pandas as pd
 from scipy import stats
 import numpy as np
 
+
 def _GLM_to_tidy_long(data, labels, glm_estimates, design_matrix):
 
     theta_estimates = np.zeros((len(labels), len(design_matrix.columns)))
@@ -17,7 +18,8 @@ def _GLM_to_tidy_long(data, labels, glm_estimates, design_matrix):
         df_estimates[idx, :] = glm_estimates[lab].df_model
         mse_estimates[idx, :] = glm_estimates[lab].MSE[0]
         for cond_idx, cond in enumerate(design_matrix.columns):
-            t_estimates[idx, cond_idx] = glm_estimates[lab].t(column=cond_idx)[matching_idx]
+            t_estimates[idx, cond_idx] = glm_estimates[lab].t(
+                column=cond_idx)[matching_idx]
             p_estimates[idx, cond_idx] = 1 - stats.t.cdf(
                 t_estimates[idx, cond_idx], df=df_estimates[idx, cond_idx])
 
@@ -25,27 +27,34 @@ def _GLM_to_tidy_long(data, labels, glm_estimates, design_matrix):
 
     for ch_idx, ch in enumerate(data.ch_names):
         for cond_idx, cond in enumerate(design_matrix.columns):
-            df = df.append(
-                {'ch_name': ch, 'condition': cond, 'variable': "theta",
-                 'value': theta_estimates[ch_idx][cond_idx]}, ignore_index=True)
-            df = df.append({'ch_name': ch, 'condition': cond, 'variable': "t",
+            df = df.append({'ch_name': ch, 'condition': cond,
+                            'variable': "theta",
+                            'value': theta_estimates[ch_idx][cond_idx]},
+                           ignore_index=True)
+            df = df.append({'ch_name': ch, 'condition': cond,
+                            'variable': "t",
                             'value': t_estimates[ch_idx][cond_idx]},
                            ignore_index=True)
-            df = df.append({'ch_name': ch, 'condition': cond, 'variable': "df",
+            df = df.append({'ch_name': ch, 'condition': cond,
+                            'variable': "df",
                             'value': df_estimates[ch_idx][cond_idx]},
                            ignore_index=True)
-            df = df.append({'ch_name': ch, 'condition': cond, 'variable': "p",
+            df = df.append({'ch_name': ch, 'condition': cond,
+                            'variable': "p",
                             'value': p_estimates[ch_idx][cond_idx]},
                            ignore_index=True)
-            df = df.append({'ch_name': ch, 'condition': cond, 'variable': "mse",
+            df = df.append({'ch_name': ch, 'condition': cond,
+                            'variable': "mse",
                             'value': mse_estimates[ch_idx][cond_idx]},
                            ignore_index=True)
 
     return df
 
+
 def _tidy_long_to_wide(d):
-    d = d.set_index(['ch_name','condition'])
-    d = d.pivot_table( columns='variable', values='value', index=['ch_name', 'condition'])
+    d = d.set_index(['ch_name', 'condition'])
+    d = d.pivot_table(columns='variable', values='value',
+                      index=['ch_name', 'condition'])
     d["Sig"] = d["p"] < 0.05
     d.reset_index(inplace=True)
     d.loc[["hbo" in s for s in d["ch_name"]], 'Chroma'] = "HbO"
