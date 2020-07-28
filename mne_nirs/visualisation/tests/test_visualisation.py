515f88eca75caaac56240f78a4dc49a8c135573d
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-07-28 21:53:09 +1000

    Test visualisation without pyvista

--- mne_nirs/visualisation/tests/test_visualisation.py
+++ mne_nirs/visualisation/tests/test_visualisation.py
@@ -12,8 +12,6 @@ from mne_nirs.experimental_design import make_first_level_design_matrix
 from mne_nirs.statistics import run_GLM
 from mne_nirs.visualisation import plot_glm_topo
 
-mne.viz.set_3d_backend('pyvista')
-
 
 @requires_pysurfer
 @traits_test
