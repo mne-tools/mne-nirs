86648f52dfdec6b1edee62baf29723d945b2b2a6
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-11-24 09:46:40 +1100

    Update setup_xvfb.sh

--- tools/setup_xvfb.sh
+++ tools/setup_xvfb.sh
@@ -1,3 +1,5 @@
+ls
+ls /bin
 #!/bin/bash -ef
 
 sudo apt-get install -yqq libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0 libopengl0
