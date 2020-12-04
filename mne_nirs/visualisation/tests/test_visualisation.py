0dbd5ccb064aad5873e7d9d261f065ec3f0bd0fb
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-12-04 14:59:07 +1100

    Test auto selection of colormaps (#209)
    * Test auto selection of colormaps
    
    * Hit positive only cmap
    
    * Test using different cmap
    
    * Test autoselection in pyvista

--- mne_nirs/visualisation/tests/test_visualisation.py
+++ mne_nirs/visualisation/tests/test_visualisation.py
@@ -32,6 +32,16 @@ def test_plot_nirs_source_detector_pyvista():
         cmap='Oranges',
         verbose=True)
 
+    mne_nirs.visualisation.plot_nirs_source_detector(
+        np.random.randn(len(raw.ch_names)) + 5,
+        raw.info, show_axes=True,
+        subject='fsaverage',
+        trans='fsaverage',
+        surfaces=['brain'],
+        fnirs=False,
+        subjects_dir=subjects_dir,
+        verbose=True)
+
 
 @requires_pysurfer
 @traits_test
@@ -48,8 +58,8 @@ def test_plot_nirs_source_detector_mayavi():
         trans='fsaverage',
         surfaces=['brain'],
         fnirs=False,
+        cmap='inferno',
         subjects_dir=subjects_dir,
-        cmap='Oranges',
         verbose=True)
 
 
