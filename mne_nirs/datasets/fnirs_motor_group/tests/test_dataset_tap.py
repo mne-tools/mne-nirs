5ecdca785a3a92d47b4ae056e05b9e7ee596d0f2
Robert Luke <hello@robertluke.net>
2020-12-06 11:36:25 +1100

    Check for sub path to ensure download was sucessful

--- mne_nirs/datasets/fnirs_motor_group/tests/test_dataset_tap.py
+++ mne_nirs/datasets/fnirs_motor_group/tests/test_dataset_tap.py
@@ -8,4 +8,4 @@ import mne_nirs
 def test_dataset_tapping_group():
     datapath = mne_nirs.datasets.fnirs_motor_group.data_path()
     assert op.isdir(datapath)
-    assert op.getsize(datapath) == 384
+    assert op.isdir(op.join(datapath, "sub-01"))
