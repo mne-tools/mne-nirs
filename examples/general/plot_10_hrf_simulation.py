1b59abea8a3741bcdb5aa2c8c30148b77cb7823d
Robert Luke <hello@robertluke.net>
2021-01-27 16:32:04 +1100

    Where did these go?

---
+++ examples/general/plot_10_hrf_simulation.py
@@ -0,0 +1,190 @@
+"""
+.. _tut-fnirs-hrf-sim:
+
+GLM Analysis (Simulated)
+=============================
+
+In this example we simulate a block design fNIRS experiment and analyse
+the simulated signal. We investigate the effect additive noise and
+measurement length has on response amplitude estimates.
+
+.. contents:: Page contents
+   :local:
+   :depth: 2
+
+"""
+# sphinx_gallery_thumbnail_number = 3
+
+# Authors: Robert Luke <mail@robertluke.net>
+#
+# License: BSD (3-clause)
+
+import mne
+import mne_nirs
+import matplotlib.pylab as plt
+import numpy as np
+from mne_nirs.experimental_design import make_first_level_design_matrix
+from mne_nirs.statistics import run_GLM
+from nilearn.plotting import plot_design_matrix
+np.random.seed(1)
+
+
+###############################################################################
+# Simulate noise free NIRS data
+# -----------------------------
+#
+# First we simulate some noise free data. We simulate 5 minutes of data with a
+# block design. The inter stimulus interval of the stimuli is uniformly
+# selected between 15 and 45 seconds.
+# The amplitude of the simulated signal is 4 uMol and the sample rate is 3 Hz.
+# The simulated signal is plotted below.
+
+sfreq = 3.
+amp = 4.
+
+raw = mne_nirs.simulation.simulate_nirs_raw(
+    sfreq=sfreq, sig_dur=60 * 5, amplitude=amp, isi_min=15., isi_max=45.)
+raw.plot(duration=300, show_scrollbars=False)
+
+
+###############################################################################
+# Create design matrix
+# ------------------------------------
+#
+# Next we create a design matrix based on the annotation times in the simulated
+# data. We use the nilearn plotting function to visualise the design matrix.
+# For more details on this procedure see :ref:`tut-fnirs-hrf`.
+
+design_matrix = make_first_level_design_matrix(raw, stim_dur=5.0,
+                                               drift_order=1,
+                                               drift_model='polynomial')
+fig, ax1 = plt.subplots(figsize=(10, 6), nrows=1, ncols=1)
+fig = plot_design_matrix(design_matrix, ax=ax1)
+
+
+###############################################################################
+# Estimate response on clean data
+# -------------------------------
+#
+# Here we run the GLM analysis on the clean data.
+# The design matrix had three columns, so we get an estimate for our simulated
+# event, the first order drift, and the constant.
+# We see that the estimate of the first component is 4e-6 (4 uM),
+# which was the amplitude we used in the simulation.
+# We also see that the mean square error of the model fit is close to zero.
+
+glm_est = run_GLM(raw, design_matrix)
+
+print("Estimate:", glm_est['Simulated'].theta[0],
+      "  MSE:", glm_est['Simulated'].MSE,
+      "  Error (uM):", 1e6*(glm_est['Simulated'].theta[0] - amp*1e-6))
+
+
+###############################################################################
+# Simulate noisy NIRS data (white)
+# --------------------------------
+#
+# Real data has noise. Here we add white noise, this noise is not realistic
+# but serves as a reference point for evaluating the estimation process.
+# We run the GLM analysis exactly as in the previous section
+# and plot the noisy data and the GLM fitted model.
+# We print the response estimate and see that is close, but not exactly correct,
+# we observe the mean square error is similar to the added noise.
+# Note that the clean data plot is so similar to the GLM estimate that it is hard to see unless zoomed in. 
+
+# First take a copy of noise free data for comparison
+raw_noise_free = raw.copy()
+
+raw._data += np.random.normal(0, np.sqrt(1e-11), raw._data.shape)
+glm_est = run_GLM(raw, design_matrix)
+
+plt.plot(raw.times, raw_noise_free.get_data().T * 1e6)
+plt.plot(raw.times, raw.get_data().T * 1e6, alpha=0.3)
+plt.plot(raw.times, glm_est['Simulated'].theta[0] * design_matrix["A"].values * 1e6)
+plt.xlabel("Time (s)")
+plt.ylabel("Haemoglobin (uM)")
+plt.legend(["Clean Data", "Noisy Data", "GLM Estimate"])
+
+print("Estimate:", glm_est['Simulated'].theta[0],
+      "  MSE:", glm_est['Simulated'].MSE,
+      "  Error (uM):", 1e6*(glm_est['Simulated'].theta[0] - amp*1e-6))
+
+
+###############################################################################
+# Simulate noisy NIRS data (colored)
+# ----------------------------------
+#
+# Here we add colored noise which better matches what is seen with real data.
+# Again, the same GLM procedure is run.
+# The estimate is reported below, and even though the signal was difficult to
+# observe in the raw data, the GLM analysis has extracted an accurate estimate.
+# However, the error is greater for the colored than white noise.
+
+raw = raw_noise_free.copy()
+cov = mne.Covariance(np.ones(1) * 1e-11, raw.ch_names,
+                     raw.info['bads'], raw.info['projs'], nfree=0)
+raw = mne.simulation.add_noise(raw, cov,
+                               iir_filter=[1., -0.58853134, -0.29575669,
+                                           -0.52246482, 0.38735476,
+                                           0.02428681])
+design_matrix = make_first_level_design_matrix(raw, stim_dur=5.0,
+                                               drift_order=1,
+                                               drift_model='polynomial')
+glm_est = run_GLM(raw, design_matrix)
+
+plt.plot(raw.times, raw_noise_free.get_data().T * 1e6)
+plt.plot(raw.times, raw.get_data().T * 1e6, alpha=0.3)
+plt.plot(raw.times, glm_est['Simulated'].theta[0] * design_matrix["A"].values * 1e6)
+plt.xlabel("Time (s)")
+plt.ylabel("Haemoglobin (uM)")
+plt.legend(["Clean Data", "Noisy Data", "GLM Estimate"])
+
+print("Estimate:", glm_est['Simulated'].theta[0],
+      "  MSE:", glm_est['Simulated'].MSE,
+      "  Error (uM):", 1e6*(glm_est['Simulated'].theta[0] - amp*1e-6))
+
+
+###############################################################################
+# How does increasing the measurement length affect estimation accuracy?
+# ----------------------------------------------------------------------
+#
+# The easiest way to reduce error in your response estimate is to collect more
+# data. Here we simulated increasing the recording time to 30 minutes.
+# We run the same analysis and observe that the error is reduced from
+# approximately 0.6 uM for 5 minutes of data to 0.25 uM for 30 minutes of data.
+
+raw = mne_nirs.simulation.simulate_nirs_raw(
+    sfreq=sfreq, sig_dur=60 * 30, amplitude=amp, isi_min=15., isi_max=45.)
+cov = mne.Covariance(np.ones(1) * 1e-11, raw.ch_names,
+                     raw.info['bads'], raw.info['projs'], nfree=0)
+raw = mne.simulation.add_noise(raw, cov,
+                               iir_filter=[1., -0.58853134, -0.29575669,
+                                           -0.52246482, 0.38735476,
+                                           0.02428681])
+
+design_matrix = make_first_level_design_matrix(raw, stim_dur=5.0,
+                                               drift_order=1,
+                                               drift_model='polynomial')
+glm_est = run_GLM(raw, design_matrix)
+
+plt.plot(raw.times, raw.get_data().T * 1e6, alpha=0.3)
+plt.plot(raw.times, glm_est['Simulated'].theta[0] * design_matrix["A"].values * 1e6)
+plt.xlabel("Time (s)")
+plt.ylabel("Haemoglobin (uM)")
+plt.legend(["Noisy Data", "GLM Estimate"])
+
+print("Estimate:", glm_est['Simulated'].theta[0],
+      "  MSE:", glm_est['Simulated'].MSE,
+      "  Error (uM):", 1e6*(glm_est['Simulated'].theta[0] - amp*1e-6))
+
+
+###############################################################################
+# Conclusion?
+# -----------
+#
+# In this example we have generated a noise free signal containing simulated
+# haemodynamic responses. We were then able to accurately estimate the amplitude
+# of the simulated signal. We then added noise and illustrated that the
+# estimate provided by the GLM was correct, but contained some error. We
+# observed that as the measurement time was increased, the estimated
+# error decreased.
---
+++ examples/general/plot_12_group_glm.py
@@ -0,0 +1,380 @@
+"""
+.. _tut-fnirs-group:
+
+Group Level GLM
+===============
+
+.. sidebar:: Relevant literature
+
+   Gorgolewski, Krzysztof J., et al.
+   "The brain imaging data structure, a format for organizing and describing
+   outputs of neuroimaging experiments." Scientific data 3.1 (2016): 1-9.
+
+   Santosa, H., Zhai, X., Fishburn, F., & Huppert, T. (2018).
+   The NIRS brain AnalyzIR toolbox. Algorithms, 11(5), 73.
+
+This is an example of a group level GLM based fNIRS analysis in MNE-NIRS.
+
+Individual level analysis of this data is described in the
+:ref:`MNE fNIRS waveform tutorial <mne:tut-fnirs-processing>`
+and the
+:ref:`MNE-NIRS fNIRS GLM tutorial <tut-fnirs-hrf>`
+So this example will skim over the individual level details
+and focus on the group level aspect of analysis.
+Here we describe how to process multiple measurements
+and summarise  group level effects both as summary statistics and visually.
+
+The data used in this example is available
+`at this location <https://github.com/rob-luke/BIDS-NIRS-Tapping>`_.
+It is a finger tapping example and is briefly described below.
+The dataset contains 5 participants.
+The example dataset is in
+`BIDS <https://bids.neuroimaging.io>`_
+format and therefore already contains
+information about triggers, condition names, etc.
+The BIDS specification for NIRS data is still under development,
+as such you must use the development branch of MNE-BIDS as listed in the
+requirements_doc.txt file to run this example.
+
+.. collapse:: |chevron-circle-down| Data description (click to expand)
+   :class: success
+
+   Optodes were placed over the motor cortex using the standard NIRX motor
+   montage, but with 8 short channels added (see their web page for details).
+   To view the sensor locations run
+   `raw_intensity.plot_sensors()`.
+   A sound was presented to indicate which hand the participant should tap.
+   Participants tapped their thumb to their fingers for 5s.
+   Conditions were presented in a random order with a randomised inter
+   stimulus interval.
+
+.. contents:: Page contents
+   :local:
+   :depth: 2
+"""
+# sphinx_gallery_thumbnail_number = 2
+
+# Authors: Robert Luke <mail@robertluke.net>
+#
+# License: BSD (3-clause)
+
+
+# Import common libraries
+import numpy as np
+import pandas as pd
+
+# Import MNE processing
+from mne.preprocessing.nirs import optical_density, beer_lambert_law
+
+# Import MNE-NIRS processing
+from mne_nirs.statistics import run_GLM
+from mne_nirs.experimental_design import make_first_level_design_matrix
+from mne_nirs.statistics import glm_region_of_interest, statsmodels_to_results
+from mne_nirs.statistics import compute_contrast
+from mne_nirs.channels import get_short_channels, get_long_channels
+from mne_nirs.channels import picks_pair_to_idx
+from mne_nirs.utils._io import glm_to_tidy
+from mne_nirs.visualisation import plot_glm_group_topo
+from mne_nirs.datasets import fnirs_motor_group
+
+# Import MNE-BIDS processing
+from mne_bids import BIDSPath, read_raw_bids
+
+# Import StatsModels
+import statsmodels.formula.api as smf
+
+# Import Plotting Library
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from lets_plot import *
+LetsPlot.setup_html()
+
+
+###############################################################################
+# Define individual analysis
+# --------------------------
+#
+# .. sidebar:: Individual analysis procedures
+#
+#    Waveform individual analysis:
+#    :ref:`MNE docs <mne:tut-fnirs-processing>`
+#
+#    GLM individual analysis:
+#    :ref:`MNE-NIRS docs <tut-fnirs-hrf>`
+#
+# First we define the analysis that will be applied to each file.
+# This is a GLM analysis as described in the
+# :ref:`individual GLM tutorial <tut-fnirs-hrf>`,
+# so this example will skim over the individual level details.
+#
+# The analysis extracts a response estimate for each channel,
+# each region of interest, and computes a contrast between left and right
+# finger tapping.
+# We return the raw object and data frames for the computed results.
+# Information about channels, triggers and their meanings are stored in the
+# BIDS structure and are automatically obtained when importing the data.
+#
+# Here we also resample to a 0.3 Hz sample rate just to speed up the example
+# and use less memory, resampling to 0.6 Hz is a better choice for full
+# analyses.
+
+
+def individual_analysis(bids_path, ID):
+
+    raw_intensity = read_raw_bids(bids_path=bids_path, verbose=False)
+
+    # Convert signal to haemoglobin and resample
+    raw_od = optical_density(raw_intensity)
+    raw_haemo = beer_lambert_law(raw_od)
+    raw_haemo.resample(0.3)
+
+    # Cut out just the short channels for creating a GLM repressor
+    sht_chans = get_short_channels(raw_haemo)
+    raw_haemo = get_long_channels(raw_haemo)
+
+    # Create a design matrix
+    design_matrix = make_first_level_design_matrix(raw_haemo, stim_dur=5.0)
+
+    # Append short channels mean to design matrix
+    design_matrix["ShortHbO"] = np.mean(sht_chans.copy().pick(picks="hbo").get_data(), axis=0)
+    design_matrix["ShortHbR"] = np.mean(sht_chans.copy().pick(picks="hbr").get_data(), axis=0)
+
+    # Run GLM
+    glm_est = run_GLM(raw_haemo, design_matrix)
+
+    # Define channels in each region of interest
+    # List the channel pairs manually
+    left = [[4, 3], [1, 3], [3, 3], [1, 2], [2, 3], [1, 1]]
+    right = [[6, 7], [5, 7], [7, 7], [5, 6], [6, 7], [5, 5]]
+    # Then generate the correct indices for each pair
+    groups = dict(
+        Left_Hemisphere=picks_pair_to_idx(raw_haemo, left, on_missing='ignore'),
+        Right_Hemisphere=picks_pair_to_idx(raw_haemo, right, on_missing='ignore'))
+
+    # Extract channel metrics
+    cha = glm_to_tidy(raw_haemo, glm_est, design_matrix)
+    cha["ID"] = ID  # Add the participant ID to the dataframe
+
+    # Compute region of interest results from channel data
+    roi = pd.DataFrame()
+    for idx, col in enumerate(design_matrix.columns):
+        roi = roi.append(glm_region_of_interest(glm_est, groups, idx, col))
+    roi["ID"] = ID  # Add the participant ID to the dataframe
+
+    # Contrast left vs right tapping
+    contrast_matrix = np.eye(design_matrix.shape[1])
+    basic_conts = dict([(column, contrast_matrix[i])
+                        for i, column in enumerate(design_matrix.columns)])
+    contrast_LvR = basic_conts['Tapping/Left'] - basic_conts['Tapping/Right']
+    contrast = compute_contrast(glm_est, contrast_LvR)
+    con = glm_to_tidy(raw_haemo, contrast, design_matrix)
+    con["ID"] = ID  # Add the participant ID to the dataframe
+
+    # Convert to uM for nicer plotting below.
+    cha["theta"] = [t * 1.e6 for t in cha["theta"]]
+    roi["theta"] = [t * 1.e6 for t in roi["theta"]]
+    con["effect"] = [t * 1.e6 for t in con["effect"]]
+
+    return raw_haemo, roi, cha, con
+
+
+###############################################################################
+# Run analysis on all participants
+# --------------------------------
+#
+# Next we loop through the five measurements and run the individual analysis
+# on each. We append the individual results in to a large dataframe that
+# will contain the results from all measurements. We create a group dataframe
+# for the region of interest, channel level, and contrast results.
+
+df_roi = pd.DataFrame()  # To store region of interest results
+df_cha = pd.DataFrame()  # To store channel level results
+df_con = pd.DataFrame()  # To store channel level contrast results
+
+for sub in range(1, 6):  # Loop from first to fifth subject
+    ID = '%02d' % sub  # Tidy the subject name
+
+    # Create path to file based on experiment info
+    bids_path = BIDSPath(subject=ID, task="tapping",
+                         root=fnirs_motor_group.data_path(),
+                         datatype="nirs", suffix="nirs", extension=".snirf")
+
+    # Analyse data and return both ROI and channel results
+    raw_haemo, roi, channel, con = individual_analysis(bids_path, ID)
+
+    # Append individual results to all participants
+    df_roi = df_roi.append(roi)
+    df_cha = df_cha.append(channel)
+    df_con = df_con.append(con)
+
+
+###############################################################################
+# Visualise Individual results
+# ----------------------------
+#
+# First we visualise the results from each individual to ensure the
+# data values look reasonable.
+# Here we see that we have data from five participants, we plot just the HbO
+# values and observe they are in the expect range.
+# We can already see that the control condition is always near zero,
+# and that the responses look to be contralateral to the tapping hand.
+
+grp_results = df_roi.query("Condition in ['Control', 'Tapping/Left', 'Tapping/Right']")
+grp_results = grp_results.query("Chroma in ['hbo']")
+
+ggplot(grp_results, aes(x='Condition', y='theta', color='ROI', shape='ROI')) \
+    + geom_hline(y_intercept=0, linetype="dashed", size=1) \
+    + geom_point(size=5) \
+    + facet_grid('ID') \
+    + ggsize(800, 300)
+
+
+###############################################################################
+# Compute group level results
+# ---------------------------
+#
+# .. sidebar:: Relevant literature
+#
+#    For an introduction to mixed effects analysis see:
+#    Winter, Bodo. "A very basic tutorial for performing linear mixed effects
+#    analyses." arXiv preprint arXiv:1308.5499 (2013).
+#
+#    For a summary of linear mixed models in python
+#    and the relation to lmer see:
+#    :ref:`statsmodels docs <statsmodels:mixedlmmod>`
+#
+#    For a summary of these models in the context of fNIRS see section 3.5 of:
+#    Santosa, H., Zhai, X., Fishburn, F., & Huppert, T. (2018).
+#    The NIRS brain AnalyzIR toolbox. Algorithms, 11(5), 73.
+#
+# Next we use a linear mixed effects model to examine the
+# relation between conditions and our response estimate (theta).
+# Combinations of 3 fixed effects will be evaluated, ROI (left vs right),
+# condition (control, tapping/left, tapping/right), and chromophore (HbO, HbR).
+# With a random effect of subject.
+# Alternatively, you could export the group dataframe (`df_roi.to_csv()`) and
+# analyse in your favorite stats program.
+#
+# We do not explore the modeling procedure in depth here as topics
+# such model selection and examining residuals are beyond the scope of
+# this example (see relevant literature).
+# Alternatively, we could use a robust linear
+# model by using the code
+# `roi_model = rlm('theta ~ -1 + ROI:Condition:Chroma', grp_results).fit()`.
+
+grp_results = df_roi.query("Condition in ['Control','Tapping/Left', 'Tapping/Right']")
+
+roi_model = smf.mixedlm("theta ~ -1 + ROI:Condition:Chroma",
+                        grp_results, groups=grp_results["ID"]).fit(method='nm')
+roi_model.summary()
+
+
+###############################################################################
+# Visualise group results
+# -----------------------
+#
+# Now we can summarise the output of the second level model.
+# This figure shows that the control condition has small responses that
+# are not significantly different to zero for both HbO
+# and HbR in both hemispheres.
+# Whereas clear significant responses are show for the two tapping conditions.
+# We also observe the the tapping response is
+# larger in the contralateral hemisphere.
+# Filled symbols represent HbO, unfilled symbols represent HbR.
+
+df = statsmodels_to_results(roi_model)
+
+ggplot(df.query("Chroma == 'hbo'"),
+       aes(x='Condition', y='Coef.', color='Significant', shape='ROI')) \
+    + geom_hline(y_intercept=0, linetype="dashed", size=1) \
+    + geom_point(size=5) \
+    + scale_shape_manual(values=[16, 17]) \
+    + ggsize(800, 300) \
+    + geom_point(data=df.query("Chroma == 'hbr'")
+                 .query("ROI == 'Left_Hemisphere'"), size=5, shape=1) \
+    + geom_point(data=df.query("Chroma == 'hbr'")
+                 .query("ROI == 'Right_Hemisphere'"), size=5, shape=2)
+
+
+###############################################################################
+# Group topographic visualisation
+# -------------------------------
+#
+# We can also view the topographic representation of the data
+# (rather than the ROI summary above).
+# Here we just plot the oxyhaemoglobin for the two tapping conditions.
+# First we compute the mixed effects model for each channel (rather
+# than region of interest as above).
+# Then we pass these results to the topomap function.
+
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 10),
+                         gridspec_kw=dict(width_ratios=[1, 1]))
+
+# Cut down the dataframe just to the conditions we are interested in
+ch_summary = df_cha.query("Condition in ['Tapping/Left', 'Tapping/Right']")
+ch_summary = ch_summary.query("Chroma in ['hbo']")
+
+# Run group level model and convert to dataframe
+ch_model = smf.mixedlm("theta ~ -1 + ch_name:Chroma:Condition",
+                       ch_summary, groups=ch_summary["ID"]).fit(method='nm')
+ch_model_df = statsmodels_to_results(ch_model)
+
+# Plot the two conditions
+plot_glm_group_topo(raw_haemo.copy().pick(picks="hbo"),
+                    ch_model_df.query("Condition in ['Tapping/Left']"),
+                    colorbar=False, axes=axes[0, 0],
+                    vmin=0, vmax=20, cmap=mpl.cm.Oranges)
+
+plot_glm_group_topo(raw_haemo.copy().pick(picks="hbo"),
+                    ch_model_df.query("Condition in ['Tapping/Right']"),
+                    colorbar=True, axes=axes[0, 1],
+                    vmin=0, vmax=20, cmap=mpl.cm.Oranges)
+
+# Cut down the dataframe just to the conditions we are interested in
+ch_summary = df_cha.query("Condition in ['Tapping/Left', 'Tapping/Right']")
+ch_summary = ch_summary.query("Chroma in ['hbr']")
+
+# Run group level model and convert to dataframe
+ch_model = smf.mixedlm("theta ~ -1 + ch_name:Chroma:Condition",
+                       ch_summary, groups=ch_summary["ID"]).fit(method='nm')
+ch_model_df = statsmodels_to_results(ch_model)
+
+# Plot the two conditions
+plot_glm_group_topo(raw_haemo.copy().pick(picks="hbr"),
+                    ch_model_df.query("Condition in ['Tapping/Left']"),
+                    colorbar=False, axes=axes[1, 0],
+                    vmin=-10, vmax=0, cmap=mpl.cm.Blues_r)
+plot_glm_group_topo(raw_haemo.copy().pick(picks="hbr"),
+                    ch_model_df.query("Condition in ['Tapping/Right']"),
+                    colorbar=True, axes=axes[1, 1],
+                    vmin=-10, vmax=0, cmap=mpl.cm.Blues_r)
+
+
+###############################################################################
+# Contrasts
+# ---------
+#
+# Finally we can examine the difference between the left and right hand
+# tapping conditions by viewing the contrast results
+# in a topographic representation.
+
+fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(6, 6))
+con_summary = df_con.query("Chroma in ['hbo']")
+
+# Run group level model and convert to dataframe
+con_model = smf.mixedlm("effect ~ -1 + ch_name:Chroma",
+                        con_summary, groups=con_summary["ID"]).fit(method='nm')
+con_model_df = statsmodels_to_results(con_model)
+
+plot_glm_group_topo(raw_haemo.copy().pick(picks="hbo"),
+                    con_model_df, colorbar=True, axes=axes)
+
+
+###############################################################################
+#
+# Or we can view only the left hemisphere for the contrast.
+# And set all channels that dont have a significant response to zero.
+#
+
+plot_glm_group_topo(raw_haemo.copy().pick(picks="hbo").pick(picks=range(10)),
+                    con_model_df, colorbar=True, threshold=True)
