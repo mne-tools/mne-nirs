680ab76e08c70eefb6c0bf6105d54eec184002bc
Robert Luke <hello@robertluke.net>
2021-01-27 19:12:39 +1100

    Dont build tags. Specify we are on dev version

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,9 @@ extensions = [
 
 smv_branch_whitelist = r'^.*$'
 # v0.0.1 config is not compatible with sphinx-multiversion, so use 2 onwards
-smv_tag_whitelist = r'^v\d+\.\d+.[2-9]$'
+# smv_tag_whitelist = r'^v\d+\.\d+.[2-9]$'
+# Just build master and no tags
+smv_tag_whitelist = None
 # Mark vX.Y.Z as releases
 smv_released_pattern = r'^refs/tags/.*$'
 
