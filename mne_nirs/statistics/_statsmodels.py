BUG: Fix precision in tables

mne_nirs/statistics/_statsmodels.py
@@ -3,10 +3,15 @@
 # License: BSD (3-clause)
 
 import pandas as pd
+import numpy as np
 
 
 def summary_to_dataframe(summary):
-    '''Convert statsmodels summary to pandas dataframe'''
+    '''Convert statsmodels summary to pandas dataframe.
+
+    .. warning:: The summary has precision issues, use the numerical values
+                 from it with caution.
+    '''
     results = summary.tables[1]
     if type(results) is not pd.core.frame.DataFrame:
         results = summary.tables[1].as_html()
@@ -53,6 +58,14 @@ def expand_summary_dataframe(summary):
     return summary
 
 
+_REPLACEMENTS = (
+    ('P>|z|', 'pvalues'),
+    ('Coef.', 'fe_params'),
+    ('z', 'tvalues'),
+    ('P>|t|', 'pvalues'),
+)
+
+
 def statsmodels_to_results(model):
     """
     Convert statsmodels summary to a dataframe.
@@ -66,6 +79,43 @@ def statsmodels_to_results(model):
     -------
     df : Pandas dataframe.
     """
+    from statsmodels.regression.mixed_linear_model import MixedLMResultsWrapper
+    from scipy.stats.distributions import norm
     df = summary_to_dataframe(model.summary())
+    # deal with numerical precision loss in at least some of the values
+    for col, attr in _REPLACEMENTS:
+        if col in df.columns:
+            df[col] = getattr(model, attr, df[col])
+
+    # This one messes up the standard error and quartiles, too
+    if isinstance(model, MixedLMResultsWrapper):
+        sl = slice(model.k_fe)
+        mu = np.asarray(df.iloc[sl, df.columns == 'Coef.'])[:, 0]
+        # Adapted from statsmodels, see
+        # https://github.com/statsmodels/statsmodels/blob/master/statsmodels/regression/mixed_linear_model.py#L2710-L2736  # noqa: E501
+        stderr = np.sqrt(np.diag(model.cov_params()[sl]))
+        df.iloc[sl, df.columns == 'Std.Err.'] = stderr
+        # Confidence intervals
+        qm = -norm.ppf(0.05 / 2)
+        df.iloc[sl, df.columns == '[0.025'] = mu - qm * stderr
+        df.iloc[sl, df.columns == '0.975]'] = mu + qm * stderr
+        # All random effects variances and covariances
+        sdf = np.zeros((model.k_re2 + model.k_vc, 2))
+        jj = 0
+        for i in range(model.k_re):
+            for j in range(i + 1):
+                sdf[jj, 0] = np.asarray(model.cov_re)[i, j]
+                sdf[jj, 1] = np.sqrt(model.scale) * model.bse[model.k_fe + jj]
+                jj += 1
+
+        # Variance components
+        for i in range(model.k_vc):
+            sdf[jj, 0] = model.vcomp[i]
+            sdf[jj, 1] = np.sqrt(model.scale) * model.bse[model.k_fe + jj]
+            jj += 1
+
+        df.iloc[model.k_fe:, df.columns == 'Coef.'] = sdf[:, 0]
+        df.iloc[model.k_fe:, df.columns == 'Std.Err.'] = sdf[:, 1]
+
     df = expand_summary_dataframe(df)
     return df