dd1a17632df612a36fc85c5fcaef2d7034bfdeb4
Robert Luke <748691+rob-luke@users.noreply.github.com>
2021-02-26 08:40:27 +1100

    Update mne_nirs/channels/tests/test_roi.py
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>

--- mne_nirs/channels/tests/test_roi.py
+++ mne_nirs/channels/tests/test_roi.py
@@ -34,8 +34,8 @@ def test_roi_picks():
 
     with pytest.warns(RuntimeWarning, match='No matching channels'):
         picks = picks_pair_to_idx(raw, [[1, 1], [1, 2], [15, 13], [8, 16]],
-                                on_missing='warning')
-        assert len(picks) == 6  # Missing should be ignored
+                                  on_missing='warning')
+    assert len(picks) == 6  # Missing should be ignored
 
     picks = picks_pair_to_idx(raw, [[1, 1], [1, 2], [15, 13], [8, 16]],
                               on_missing='ignore')
