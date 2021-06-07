083a4fa1cb2653e0fa2648db2b90a2c136d39f0f
Robert Luke <748691+rob-luke@users.noreply.github.com>
2021-06-07 20:14:04 +1000

    Add ability to use AR(N) noise models (#277)

--- mne_nirs/statistics/tests/test_statistics.py
+++ mne_nirs/statistics/tests/test_statistics.py
@@ -3,11 +3,17 @@
 # License: BSD (3-clause)
 
 import pytest
+import numpy as np
 import nilearn
+
+from mne import Covariance
+from mne.simulation import add_noise
 
 from mne_nirs.experimental_design import make_first_level_design_matrix
 from mne_nirs.statistics import run_GLM
 from mne_nirs.simulation import simulate_nirs_raw
+
+iir_filter = [1., -0.58853134, -0.29575669, -0.52246482, 0.38735476, 0.024286]
 
 
 @pytest.mark.filterwarnings('ignore:.*nilearn.glm module is experimental.*:')
@@ -27,3 +33,39 @@ def test_run_GLM():
     _, ni_est = nilearn.glm.first_level.run_glm(
         raw.get_data(0).T, design_matrix.values)
     assert type(ni_est) == type(glm_estimates)
+
+
+def test_run_GLM_order():
+    raw = simulate_nirs_raw(sig_dur=200, stim_dur=5., sfreq=3)
+    design_matrix = make_first_level_design_matrix(raw, stim_dur=5.,
+                                                   drift_order=1,
+                                                   drift_model='polynomial')
+
+    # Default should be first order AR
+    glm_estimates = run_GLM(raw, design_matrix)
+    assert glm_estimates['Simulated'].model.order == 1
+
+    # Default should be first order AR
+    glm_estimates = run_GLM(raw, design_matrix, noise_model='ar2')
+    assert glm_estimates['Simulated'].model.order == 2
+
+    glm_estimates = run_GLM(raw, design_matrix, noise_model='ar7')
+    assert glm_estimates['Simulated'].model.order == 7
+
+    # Auto should be 4 times sample rate
+    cov = Covariance(np.ones(1) * 1e-11, raw.ch_names,
+                     raw.info['bads'], raw.info['projs'], nfree=0)
+    raw = add_noise(raw, cov, iir_filter=iir_filter)
+    glm_estimates = run_GLM(raw, design_matrix, noise_model='auto')
+    assert glm_estimates['Simulated'].model.order == 3 * 4
+
+    raw = simulate_nirs_raw(sig_dur=10, stim_dur=5., sfreq=2)
+    cov = Covariance(np.ones(1) * 1e-11, raw.ch_names,
+                     raw.info['bads'], raw.info['projs'], nfree=0)
+    raw = add_noise(raw, cov, iir_filter=iir_filter)
+    design_matrix = make_first_level_design_matrix(raw, stim_dur=5.,
+                                                   drift_order=1,
+                                                   drift_model='polynomial')
+    # Auto should be 4 times sample rate
+    glm_estimates = run_GLM(raw, design_matrix, noise_model='auto')
+    assert glm_estimates['Simulated'].model.order == 2 * 4
