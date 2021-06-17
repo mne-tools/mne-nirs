c8979d0f04c1036574495fa5564cb5547a29906e
Robert Luke <hello@robertluke.net>
2021-06-18 08:41:11 +1000

    General tutorial improvements

--- examples/general/plot_11_hrf_measured.py
+++ examples/general/plot_11_hrf_measured.py
@@ -256,6 +256,33 @@ plt.ylabel("Amplitude")
 data_subset = raw_haemo.copy().pick(picks=range(2))
 glm_est = run_GLM(data_subset, design_matrix)
 
+###############################################################################
+#
+# This returns a GLM regression estimate for each channel.
+# This data is stored in a dictionary, which can be addressed using the
+# desired channel name. You can view an overview of the estimates as:
+
+glm_est
+
+
+###############################################################################
+#
+# Or you can view the estimate for a single channel by indexing it by name.
+# You can see that for each channel a standard
+# `Nilearn RegressionResults object <https://nilearn.github.io/modules/generated/nilearn.glm.RegressionResults.html>`_
+# is returned. These objects are rich with information that can be requested
+# from the object, for example to determine the mean square error of the
+# estimate you would call
+
+glm_est['S1_D1 hbo'].MSE
+
+
+###############################################################################
+#
+# Due to the richness of the object we provide a function to extract commonly
+# used information and put it in a convenient dataframe/table.
+
+glm_to_tidy(data_subset, glm_est, design_matrix)
 
 ###############################################################################
 #
