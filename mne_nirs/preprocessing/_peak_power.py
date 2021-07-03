cca416ae8b0e5017f9a48edc0dcbdee0efe9a1c6
Eric Larson <larson.eric.d@gmail.com>
2021-07-03 01:51:26 -0400

    MAINT: Update for Hitachi (#305)

--- mne_nirs/preprocessing/_peak_power.py
+++ mne_nirs/preprocessing/_peak_power.py
@@ -9,8 +9,7 @@ from scipy.signal import periodogram
 from mne import pick_types
 from mne.io import BaseRaw
 from mne.utils import _validate_type, verbose
-from mne.preprocessing.nirs import (_channel_frequencies,
-                                    _check_channels_ordered)
+from mne.preprocessing.nirs import _validate_nirs_info
 from mne.filter import filter_data
 
 
@@ -61,8 +60,7 @@ def peak_power(raw, time_window=10, threshold=0.1, l_freq=0.7, h_freq=1.5,
         raise RuntimeError('Scalp coupling index '
                            'should be run on optical density data.')
 
-    freqs = np.unique(_channel_frequencies(raw.info))
-    picks = _check_channels_ordered(raw.info, freqs)
+    picks = _validate_nirs_info(raw.info)
 
     filtered_data = filter_data(raw._data, raw.info['sfreq'], l_freq, h_freq,
                                 picks=picks, verbose=verbose,
@@ -89,8 +87,9 @@ def peak_power(raw, time_window=10, threshold=0.1, l_freq=0.7, h_freq=1.5,
             c1 = filtered_data[ii][start_sample:end_sample]
             c2 = filtered_data[ii + 1][start_sample:end_sample]
 
-            c1 = c1 / np.std(c1)
-            c2 = c2 / np.std(c2)
+            # protect against zero
+            c1 = c1 / (np.std(c1) or 1)
+            c2 = c2 / (np.std(c2) or 1)
 
             c = np.correlate(c1, c2, "full")
             c = c / (window_samples)
