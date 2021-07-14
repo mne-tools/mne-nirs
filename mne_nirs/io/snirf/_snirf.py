3098cc978da0f46fc9e607569187d422facced84
Darin Erat Sleiter <darin@ae.studio>
2021-07-14 18:32:29 +1000

    Fix all inconsistencies listed in #317

--- mne_nirs/io/snirf/_snirf.py
+++ mne_nirs/io/snirf/_snirf.py
@@ -21,7 +21,7 @@ def write_raw_snirf(raw, fname):
         Path to the SNIRF data file.
     """
 
-    picks = _picks_to_idx(raw.info, 'fnirs_cw_amplitude', exclude=[])
+    picks = _picks_to_idx(raw.info, "fnirs_cw_amplitude", exclude=[])
     assert len(picks) == len(raw.ch_names), "Data must be fnirs_cw_amplitude"
 
     # Reordering channels
@@ -31,11 +31,23 @@ def write_raw_snirf(raw, fname):
 
     with h5py.File(fname, "w") as f:
         nirs = f.create_group("/nirs")
+        f.create_dataset("formatVersion", data=[_str_encode("1.0")])
 
         _add_metadata_tags(raw, nirs)
         _add_single_data_block(raw, nirs)
         _add_probe_info(raw, nirs)
         _add_stim_info(raw, nirs)
+
+
+def _str_encode(str_val):
+    """Encode a string for use in an h5py Dataset.
+
+    Parameters
+    ----------
+    str_val : str
+        The string to encode.
+    """
+    return str_val.encode("UTF-8")
 
 
 def _add_metadata_tags(raw, nirs):
@@ -62,37 +74,28 @@ def _add_metadata_tags(raw, nirs):
     metadata_tags.create_dataset("DateOfBirth", data=[_str_encode(birthstr)])
 
     # Store demographic info
-    subject_id = raw.info["subject_info"]['first_name']
+    subject_id = raw.info["subject_info"]["first_name"]
     metadata_tags.create_dataset("SubjectID", data=[_str_encode(subject_id)])
 
     # Store the units of measurement
-    metadata_tags.create_dataset("LengthUnit", data=[_str_encode('m')])
+    metadata_tags.create_dataset("LengthUnit", data=[_str_encode("m")])
+    metadata_tags.create_dataset("TimeUnit", data=[_str_encode("s")])
+    metadata_tags.create_dataset("FrequencyUnit", data=[_str_encode("Hz")])
 
     # Add non standard (but allowed) custom metadata tags
-    if 'middle_name' in raw.info["subject_info"]:
-        middle_name = raw.info["subject_info"]['middle_name']
+    if "middle_name" in raw.info["subject_info"]:
+        middle_name = raw.info["subject_info"]["middle_name"]
         metadata_tags.create_dataset("middleName",
                                      data=[_str_encode(middle_name)])
-    if 'last_name' in raw.info["subject_info"]:
-        last_name = raw.info["subject_info"]['last_name']
+    if "last_name" in raw.info["subject_info"]:
+        last_name = raw.info["subject_info"]["last_name"]
         metadata_tags.create_dataset("lastName", data=[_str_encode(last_name)])
-    if 'sex' in raw.info["subject_info"]:
-        sex = str(int(raw.info["subject_info"]['sex']))
+    if "sex" in raw.info["subject_info"]:
+        sex = str(int(raw.info["subject_info"]["sex"]))
         metadata_tags.create_dataset("sex", data=[_str_encode(sex)])
-    if raw.info['dig'] is not None:
-        coord_frame_id = int(raw.info['dig'][0].get("coord_frame"))
+    if raw.info["dig"] is not None:
+        coord_frame_id = int(raw.info["dig"][0].get("coord_frame"))
         metadata_tags.create_dataset("MNE_coordFrame", data=[coord_frame_id])
-
-
-def _str_encode(str_val):
-    """Encode a string for use in an h5py Dataset.
-
-    Parameters
-    ----------
-    str_val : str
-        The string to encode.
-    """
-    return str_val.encode('UTF-8')
 
 
 def _add_single_data_block(raw, nirs):
@@ -125,15 +128,12 @@ def _add_measurement_lists(raw, data_block):
     data_block : hpy5.Group
         The hdf5 data1 group to which the measurement lists should be added.
     """
-    # Prep data for storing each MNE channel as SNIRF measurementList
-    data_block.create_dataset("measurementList1/dataType", data=1)
-
     sources = _get_source_list(raw)
     detectors = _get_detector_list(raw)
     wavelengths = _get_wavelength_list(raw)
 
     for idx, ch_name in enumerate(raw.ch_names, start=1):
-        ml_id = f'measurementList{idx}'
+        ml_id = f"measurementList{idx}"
         ch_group = data_block.require_group(ml_id)
 
         source_idx = sources.index(_extract_source(ch_name)) + 1
@@ -142,6 +142,10 @@ def _add_measurement_lists(raw, data_block):
         ch_group.create_dataset("sourceIndex", data=[source_idx])
         ch_group.create_dataset("detectorIndex", data=[detector_idx])
         ch_group.create_dataset("wavelengthIndex", data=[wavelength_idx])
+
+        # Set dataType and dataTypeIndex for CW Amplitude measurements
+        ch_group.create_dataset("dataType", data=1)
+        ch_group.create_dataset("dataTypeIndex", data=1)
 
 
 def _add_probe_info(raw, nirs):
@@ -161,8 +165,8 @@ def _add_probe_info(raw, nirs):
     probe = nirs.create_group("probe")
 
     # Store source/detector/wavelength info
-    encoded_source_labels = [_str_encode(f'S{src}') for src in sources]
-    encoded_detector_labels = [_str_encode(f'D{det}') for det in detectors]
+    encoded_source_labels = [_str_encode(f"S{src}") for src in sources]
+    encoded_detector_labels = [_str_encode(f"D{det}") for det in detectors]
     probe.create_dataset("sourceLabels", data=encoded_source_labels)
     probe.create_dataset("detectorLabels", data=encoded_detector_labels)
     probe.create_dataset("wavelengths", data=wavelengths)
@@ -174,15 +178,15 @@ def _add_probe_info(raw, nirs):
     detlocs = np.empty((len(detectors), 3))
     for i, src in enumerate(sources):
         idx = ch_sources.index(float(src))
-        srclocs[i, :] = raw.info['chs'][idx]['loc'][3:6]
+        srclocs[i, :] = raw.info["chs"][idx]["loc"][3:6]
     for i, det in enumerate(detectors):
         idx = ch_detectors.index(float(det))
-        detlocs[i, :] = raw.info['chs'][idx]['loc'][6:9]
+        detlocs[i, :] = raw.info["chs"][idx]["loc"][6:9]
     probe.create_dataset("sourcePos3D", data=srclocs)
     probe.create_dataset("detectorPos3D", data=detlocs)
 
     # Store probe landmarks
-    if raw.info['dig'] is not None:
+    if raw.info["dig"] is not None:
         _store_probe_landmarks(raw, probe)
 
 
@@ -196,9 +200,9 @@ def _store_probe_landmarks(raw, probe):
     probe : hpy5.Group
         The hdf5 probe group to which the landmark info should be added.
     """
-    diglocs = np.empty((len(raw.info['dig']), 3))
+    diglocs = np.empty((len(raw.info["dig"]), 3))
     digname = list()
-    for idx, dig in enumerate(raw.info['dig']):
+    for idx, dig in enumerate(raw.info["dig"]):
         ident = re.match(r"\d+ \(FIFFV_POINT_(\w+)\)",
                          str(dig.get("ident")))
         if ident is not None:
@@ -222,15 +226,16 @@ def _add_stim_info(raw, nirs):
         The root hdf5 nirs group to which the stimuli info should be added.
     """
     # Convert MNE annotations to SNIRF stims
-    for desc in np.unique(raw.annotations.description):
-        key = "stim" + desc
+    for idx, desc in enumerate(np.unique(raw.annotations.description), start=1):
+        stim_group = nirs.create_group(f"stim{idx}")
         trgs = np.where(raw.annotations.description == desc)[0]
         stims = np.zeros((len(trgs), 3))
         for idx, trg in enumerate(trgs):
             stims[idx, :] = [raw.annotations.onset[trg], 5.0,
                              raw.annotations.duration[trg]]
-        stim_group = nirs.create_group(key)
-        stim_group.create_dataset('data', data=stims)
+        
+        stim_group.create_dataset("data", data=stims)
+        stim_group.create_dataset("name", data=[_str_encode(desc)])
 
 
 def _get_source_list(raw):
@@ -249,17 +254,17 @@ def _get_wavelength_list(raw):
 
 
 def _match_channel_pattern(channel_name):
-    rgx = r'^S(?P<source>\d+)_D(?P<detector>\d+) (?P<wavelength>\d+)$'
+    rgx = r"^S(?P<source>\d+)_D(?P<detector>\d+) (?P<wavelength>\d+)$"
     return re.fullmatch(rgx, channel_name)
 
 
 def _extract_source(channel_name):
-    return int(_match_channel_pattern(channel_name).group('source'))
+    return int(_match_channel_pattern(channel_name).group("source"))
 
 
 def _extract_detector(channel_name):
-    return int(_match_channel_pattern(channel_name).group('detector'))
+    return int(_match_channel_pattern(channel_name).group("detector"))
 
 
 def _extract_wavelength(channel_name):
-    return float(_match_channel_pattern(channel_name).group('wavelength'))
+    return float(_match_channel_pattern(channel_name).group("wavelength"))
