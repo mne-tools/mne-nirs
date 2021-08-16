08416bad23ddbfce18640e78f8b8f79ac4997bd7
Robert Luke <748691+rob-luke@users.noreply.github.com>
2021-08-17 08:21:49 +1000

    Import fix (#353)

--- mne_nirs/__init__.py
+++ mne_nirs/__init__.py
@@ -2,10 +2,13 @@ from ._version import __version__
 
 from . import channels
 from . import datasets
-from . import signal_enhancement
-from . import visualisation
 from . import experimental_design
+from . import io
+from . import preprocessing
+from . import signal_enhancement
 from . import simulation
+from . import statistics
 from . import utils
+from . import visualisation
 
 __all__ = ['__version__']
