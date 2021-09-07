0c5933feafb2488c1290f27ce40d6a034aa92798
Robert Luke <hello@robertluke.net>
2021-09-07 15:12:32 +1000

    Flake fixes

--- mne_nirs/io/fold/_fold.py
+++ mne_nirs/io/fold/_fold.py
@@ -3,8 +3,6 @@ import numpy as np
 import mne
 
 
-fname = "/Users/rluke/Downloads/10-10.xls"
-
 def _read_fold_xls(fname, atlas="Juelich"):
 
     page_reference = {"AAL2": 2,
@@ -13,7 +11,8 @@ def _read_fold_xls(fname, atlas="Juelich"):
                       "Juelich": 11,
                       "Loni": 14}
 
-    tbl = pd.read_excel(fname, sheet_name = page_reference[atlas])
+    tbl = pd.read_excel(fname,
+                        sheet_name=page_reference[atlas])
 
     # Remove the spacing between rows
     empty_rows = np.where(np.isnan(tbl["Specificity"]))[0]
@@ -24,15 +23,18 @@ def _read_fold_xls(fname, atlas="Juelich"):
         for col in tbl.columns:
             if not isinstance(tbl[col][row_idx], str):
                 if np.isnan(tbl[col][row_idx]):
-                    tbl[col][row_idx] = tbl[col][row_idx-1]
+                    tbl[col][row_idx] = \
+                        tbl[col][row_idx - 1]
 
     return tbl
 
 
 def _generate_all_locations():
 
-    montage = mne.channels.make_standard_montage('standard_1020', head_size=0.1)
-    coords = pd.DataFrame.from_dict(montage.get_positions()['ch_pos']).T
+    montage = mne.channels.make_standard_montage(
+        'standard_1020', head_size=0.1)
+    coords = pd.DataFrame.from_dict(
+        montage.get_positions()['ch_pos']).T
     coords["label"] = coords.index
     coords = coords.rename(columns={0: "x", 1: "y", 2: "z"})
 
@@ -44,13 +46,11 @@ def _find_closest_standard_location(x, y, z, reference):
     dists = np.zeros(reference["x"].shape)
     p0 = np.array([x, y, z])
     for idx in range(len(dists)):
-        p1 = np.array([reference["x"][idx], reference["y"][idx], reference["z"][idx]])
-        dists[idx] = np.linalg.norm(p0 -  p1)
+        p1 = np.array([reference["x"][idx],
+                       reference["y"][idx],
+                       reference["z"][idx]])
+        dists[idx] = np.linalg.norm(p0 - p1)
 
     min_idx = np.argmin(dists)
 
     return reference["label"][min_idx]
-
-tbl = _read_fold_xls(fname)
-
-
--- mne_nirs/io/fold/tests/test_fold.py
+++ mne_nirs/io/fold/tests/test_fold.py
@@ -3,19 +3,28 @@
 #          simplified BSD-3 license
 
 import os.path as op
-from numpy.testing import assert_allclose
-import pytest
+
+import pandas as pd
 
 from mne.datasets.testing import data_path, requires_testing_data
 from mne.io import read_raw_nirx
 from mne.transforms import apply_trans, _get_trans
 
-from mne_nirs.io.fold._fold import _generate_all_locations, _find_closest_standard_location
+from mne_nirs.io.fold._fold import _generate_all_locations,\
+    _find_closest_standard_location, _read_fold_xls
 
 
 # https://github.com/mne-tools/mne-testing-data/pull/72
 fname_nirx_15_3_short = op.join(data_path(download=False),
                                 'NIRx', 'nirscout', 'nirx_15_3_recording')
+
+
+def test_fold_reader():
+    tbl = _read_fold_xls("./data/example.xls", atlas="Juelich")
+    assert isinstance(tbl, pd.DataFrame)
+    assert tbl.shape == (11, 10)
+    assert "L Superior Frontal Gyrus" in \
+           list(tbl["Landmark"])
 
 
 @requires_testing_data
@@ -29,83 +38,83 @@ def test_label_finder():
     # Test central head position source
 
     raw_cz = raw.copy().pick(25)
-    raw_cz.plot_sensors()
     source_locs = raw_cz.info['chs'][0]['loc'][3:6]
 
     mni_locs = apply_trans(head_mri_t, source_locs)
     x = mni_locs[0]
     y = mni_locs[1]
     z = mni_locs[2]
-    assert _find_closest_standard_location(x, y, z, reference_locations) == "Cz"
+    assert _find_closest_standard_location(
+        x, y, z, reference_locations) == "Cz"
 
     # Test right auditory position detector
 
     raw_cz = raw.copy().pick(4)
-    raw_cz.plot_sensors()
     source_locs = raw_cz.info['chs'][0]['loc'][6:9]
 
     mni_locs = apply_trans(head_mri_t, source_locs)
     x = mni_locs[0]
     y = mni_locs[1]
     z = mni_locs[2]
-    assert _find_closest_standard_location(x, y, z, reference_locations) == "T8"
+    assert _find_closest_standard_location(
+        x, y, z, reference_locations) == "T8"
 
     # Test right auditory position source
 
     raw_cz = raw.copy().pick(4)
-    raw_cz.plot_sensors()
     source_locs = raw_cz.info['chs'][0]['loc'][3:6]
 
     mni_locs = apply_trans(head_mri_t, source_locs)
     x = mni_locs[0]
     y = mni_locs[1]
     z = mni_locs[2]
-    assert _find_closest_standard_location(x, y, z, reference_locations) == "TP8"
+    assert _find_closest_standard_location(
+        x, y, z, reference_locations) == "TP8"
 
     # Test left auditory position source
 
     raw_cz = raw.copy().pick(1)
-    raw_cz.plot_sensors()
     source_locs = raw_cz.info['chs'][0]['loc'][3:6]
 
     mni_locs = apply_trans(head_mri_t, source_locs)
     x = mni_locs[0]
     y = mni_locs[1]
     z = mni_locs[2]
-    assert _find_closest_standard_location(x, y, z, reference_locations) == "T7"
+    assert _find_closest_standard_location(
+        x, y, z, reference_locations) == "T7"
 
     # Test left auditory position detector
 
     raw_cz = raw.copy().pick(1)
-    raw_cz.plot_sensors()
     source_locs = raw_cz.info['chs'][0]['loc'][6:9]
 
     mni_locs = apply_trans(head_mri_t, source_locs)
     x = mni_locs[0]
     y = mni_locs[1]
     z = mni_locs[2]
-    assert _find_closest_standard_location(x, y, z, reference_locations) == "TP7"
+    assert _find_closest_standard_location(
+        x, y, z, reference_locations) == "TP7"
 
     # Test front position detector
 
     raw_cz = raw.copy().pick(14)
-    raw_cz.plot_sensors()
     source_locs = raw_cz.info['chs'][0]['loc'][6:9]
 
     mni_locs = apply_trans(head_mri_t, source_locs)
     x = mni_locs[0]
     y = mni_locs[1]
     z = mni_locs[2]
-    assert _find_closest_standard_location(x, y, z, reference_locations) == "AF2"
+    assert _find_closest_standard_location(
+        x, y, z, reference_locations) == "AF2"
 
     # Test rear position detector
 
     raw_cz = raw.copy().pick(9)
-    raw_cz.plot_sensors()
     source_locs = raw_cz.info['chs'][0]['loc'][6:9]
 
     mni_locs = apply_trans(head_mri_t, source_locs)
     x = mni_locs[0]
     y = mni_locs[1]
     z = mni_locs[2]
-    assert _find_closest_standard_location(x, y, z, reference_locations) == "PO2"
+    assert _find_closest_standard_location(
+        x, y, z, reference_locations) == "PO2"
