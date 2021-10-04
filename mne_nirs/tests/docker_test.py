72cd2804f44544d05e5da0a75649437d16d941d4
Robert Luke <748691+rob-luke@users.noreply.github.com>
2021-10-04 12:06:52 +1100

    Update docker_test.py

--- mne_nirs/tests/docker_test.py
+++ mne_nirs/tests/docker_test.py
@@ -8,7 +8,7 @@ renderer = mne.viz.backends.renderer.create_3d_figure(
 mne.viz.set_3d_backend("pyvista")
 print("Creating image")
 renderer.sphere((0, 0, 0), "k", 1, resolution=1000)
-renderer.plotter.camera.enable_parallel_projection(True)
+renderer.plotter.camera.enable_parallel_projection()
 renderer.figure.plotter.camera.SetParallelScale(1)
 renderer.show()
 data = (renderer.screenshot() / 255.0).mean(-1)  # colors
