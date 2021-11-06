9821222d127b61a317ec2342214a72568510ca81
Robert Luke <hello@robertluke.net>
2021-11-06 13:40:16 +1100

    Fix bug

--- examples/general/plot_50_decoding.py
+++ examples/general/plot_50_decoding.py
@@ -42,7 +42,6 @@ MNE-Python, SK-Learn, and MNE-NIRS.
 
 # Import common libraries
 import numpy as np
-import pandas as pd
 import os
 
 from sklearn.pipeline import make_pipeline
@@ -63,25 +62,9 @@ from mne import Epochs, events_from_annotations
 # Import MNE-NIRS processing
 from mne_nirs.statistics import run_glm
 from mne_nirs.experimental_design import make_first_level_design_matrix
-from mne_nirs.statistics import statsmodels_to_results
-from mne_nirs.channels import get_short_channels, get_long_channels
-from mne_nirs.channels import picks_pair_to_idx
-from mne_nirs.visualisation import plot_glm_group_topo
-from mne_nirs.datasets import fnirs_motor_group
-from mne_nirs.visualisation import plot_glm_surface_projection
-from mne_nirs.io.fold import fold_landmark_specificity, fold_channel_specificity
 
 # Import MNE-BIDS processing
 from mne_bids import BIDSPath, read_raw_bids, get_entity_vals
-
-# Import StatsModels
-import statsmodels.formula.api as smf
-
-# Import Plotting Library
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-from lets_plot import *
-LetsPlot.setup_html()
 
 
 # %%
@@ -152,9 +135,9 @@ def epoch_preprocessing(bids_path):
 # will contain the results from all measurements. We create a group dataframe
 # for the region of interest, channel level, and contrast results.
 
-st_scores = []
-
 for chroma in ['hbo', 'hbr']:
+
+    st_scores = []
     for sub in subjects:
 
         bids_path = dataset.update(subject=sub)
