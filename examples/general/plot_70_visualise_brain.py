07716d8be9e821df54e37acbc875fd4be72c40fa
Robert Luke <748691+rob-luke@users.noreply.github.com>
2021-12-01 09:16:40 +1100

    Update plot_70_visualise_brain.py

--- examples/general/plot_70_visualise_brain.py
+++ examples/general/plot_70_visualise_brain.py
@@ -47,6 +47,7 @@ from mne_nirs.statistics import run_glm
 from mne_nirs.channels import get_long_channels, get_short_channels
 from mne_nirs.io.fold import fold_landmark_specificity
 from mne_nirs.visualisation import plot_nirs_source_detector
+from mne_nirs.datasets import fnirs_motor_group
 
 
 # %%
