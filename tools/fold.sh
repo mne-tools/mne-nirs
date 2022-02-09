5b1dbd60a741aa81d9f1f9dbbc50d337371114c7
Eric Larson <larson.eric.d@gmail.com>
2022-02-09 16:02:06 -0500

    FIX: Fine

--- tools/fold.sh
+++ tools/fold.sh
@@ -2,7 +2,9 @@
 
 mkdir -p ~/mne_data/fOLD
 cd ~/mne_data/fOLD
-wget -nc -O - https://github.com/nirx/fOLD-public/archive/master.zip | gunzip -
+curl -L https://github.com/nirx/fOLD-public/archive/master.zip --output data.zip
+unzip data.zip
+rm data.zip
 if [ "${CIRCLECI}" == "true" ]; then
     echo "export MNE_FNIRS_FOLD_PATH=~/mne_data/fOLD/fOLD-public-master/Supplementary";
 elif [ "${GITHUB_ACTIONS}" == "true" ]; then
