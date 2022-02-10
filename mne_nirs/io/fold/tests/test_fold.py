ea1ba844149a677fcea010b81ecac0a82eb14d10
Eric Larson <larson.eric.d@gmail.com>
2022-02-10 15:34:40 -0500

    WIP: Ensure we always get at least some entry

--- mne_nirs/io/fold/tests/test_fold.py
+++ mne_nirs/io/fold/tests/test_fold.py
@@ -7,9 +7,12 @@ from pathlib import Path
 from shutil import copyfile
 
 import numpy as np
+from numpy.testing import assert_allclose
 import pandas as pd
 import pytest
 
+from mne.channels import make_standard_montage
+from mne.channels.montage import transform_to_head
 from mne.datasets.testing import data_path, requires_testing_data
 from mne.io import read_raw_nirx
 from mne.utils import check_version
@@ -35,7 +38,7 @@ requires_xlrd = pytest.mark.skipif(
 def test_channel_specificity(monkeypatch, tmp_path, fold_files):
     raw = read_raw_nirx(fname_nirx_15_3_short, preload=True)
     raw.pick(range(2))
-    kwargs = dict()
+    kwargs = dict(montage='standard_1020')
     n_want = 6
     if fold_files is list:
         kwargs = dict(fold_files=[foldfile])
@@ -45,7 +48,7 @@ def test_channel_specificity(monkeypatch, tmp_path, fold_files):
     else:
         assert fold_files is None
         monkeypatch.setenv('MNE_NIRS_FOLD_PATH', str(tmp_path))
-        assert len(kwargs) == 0
+        assert len(kwargs) == 1
         with pytest.raises(FileNotFoundError, match=r'fold_files\[0\] does.*'):
             fold_channel_specificity(raw)
         n_want *= 2
@@ -54,13 +57,35 @@ def test_channel_specificity(monkeypatch, tmp_path, fold_files):
     res = fold_channel_specificity(raw, **kwargs)
     assert len(res) == 2
     assert res[0].shape == (n_want, 10)
+    montage = make_standard_montage('standard_1020')
+    S, D = raw.ch_names[0].split()[0].split('_')
+    assert S == 'S1' and D == 'D2'
+    montage.rename_channels({'PO8': S, 'P6': D})  # not in the tables!
+    s_want = [0.053356, -0.070542, 0.054083]
+    d_want = [0.065061, -0.047959, 0.076765]
+    raw.set_montage(montage)
+    montage = transform_to_head(montage)
+    assert_allclose(montage._get_ch_pos()['S1'], s_want, atol=1e-6)
+    assert_allclose(montage._get_ch_pos()['D2'], d_want, atol=1e-6)
+    for ch in raw.info['chs']:
+        assert_allclose(ch['loc'][3:6], s_want, atol=1e-6)
+        assert_allclose(ch['loc'][6:9], d_want, atol=1e-6)
+    # TODO: This is wrong, should be P08 not P08h, and distance should be 0 mm!
+    with pytest.warns(RuntimeWarning, match='.*PO8h?/P6.*TP8/T8.*'):
+        res_1 = fold_channel_specificity(raw, **kwargs)[0]
+    montage.rename_channels({S: D, D: S})  # reversed
+    with pytest.warns(RuntimeWarning, match='.*PO8h?/P6.*TP8/T8.*'):
+        res_2 = fold_channel_specificity(raw, **kwargs)[0]
+    # We should check the whole thing, but this is probably good enough
+    assert (res_1['Specificity'] == res_2['Specificity']).all()
 
 
 @requires_xlrd
 def test_landmark_specificity():
     raw = read_raw_nirx(fname_nirx_15_3_short, preload=True)
-    res = fold_landmark_specificity(raw, "L Superior Frontal Gyrus",
-                                    [foldfile])
+    with pytest.warns(RuntimeWarning, match='No fOLD table entry'):
+        res = fold_landmark_specificity(raw, "L Superior Frontal Gyrus",
+                                        [foldfile])
     assert len(res) == len(raw.ch_names)
     assert np.max(res) <= 100
     assert np.min(res) >= 0
@@ -70,7 +95,7 @@ def test_landmark_specificity():
 def test_fold_workflow():
     # Read raw data
     raw = read_raw_nirx(fname_nirx_15_3_short, preload=True)
-    reference_locations = _generate_montage_locations()
+    reference_locations = _generate_montage_locations('standard_1020')
     channel_of_interest = raw.copy().pick(1)
 
     # Get source and detector labels
@@ -107,7 +132,7 @@ def test_fold_reader():
 def test_label_finder():
     """Test locating labels."""
     raw = read_raw_nirx(fname_nirx_15_3_short, preload=True)
-    reference_locations = _generate_montage_locations()
+    reference_locations = _generate_montage_locations('standard_1020')
 
     # Test central head position source
     raw_cz = raw.copy().pick(25)
