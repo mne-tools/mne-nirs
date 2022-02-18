6ad9074c4cf02be831566e2e406851cff037727e
Eric Larson <larson.eric.d@gmail.com>
2022-02-18 14:53:47 -0500

    FIX: ignore

--- mne_nirs/fixes.py
+++ mne_nirs/fixes.py
@@ -8,7 +8,8 @@ import mne
 from mne.fixes import _compare_version
 
 # Deal with fetch_data str->Path deprecation
-# This can go away once we only support 1.1
+# This can go away once we only support 1.1, along with the
+# conftest.py ignore of "data_path functions now return"
 try:
     from mne.datasets.utils import _mne_path
 except Exception:
