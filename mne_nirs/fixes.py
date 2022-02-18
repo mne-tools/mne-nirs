20290cb3e408ab100092b463c91f55bc08ac037c
Eric Larson <larson.eric.d@gmail.com>
2022-02-18 11:55:21 -0800

    FIX: LGTM

--- mne_nirs/fixes.py
+++ mne_nirs/fixes.py
@@ -11,7 +11,7 @@ from mne.fixes import _compare_version
 # This can go away once we only support 1.1, along with the
 # conftest.py ignore of "data_path functions now return"
 try:
-    from mne.datasets.utils import _mne_path
+    from mne.datasets.utils import _mne_path  # noqa
 except Exception:
     if _compare_version(mne.__version__, '>=', '1.0'):
         _mne_path = Path
