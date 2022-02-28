64ad589b9d3f1aeb2a0048919b2c75e8dc1e9d06
Eric Larson <larson.eric.d@gmail.com>
2022-02-28 17:40:30 -0500

    FIX: Windows

--- mne_nirs/conftest.py
+++ mne_nirs/conftest.py
@@ -3,7 +3,9 @@
 #
 # License: BSD-3-Clause
 
+import os
 import warnings
+from unittest import mock
 
 import pytest
 import mne
@@ -78,9 +80,25 @@ def close_all():
     plt.close('all')
 
 
+# We can't use monkeypatch because its scope (function-level) conflicts with
+# the requests fixture (module-level), so we live with a module-scoped version
+# that uses mock
+@pytest.fixture(scope='module')
+def options_3d():
+    """Disable advanced 3d rendering."""
+    with mock.patch.dict(
+        os.environ, {
+            "MNE_3D_OPTION_ANTIALIAS": "false",
+            "MNE_3D_OPTION_DEPTH_PEELING": "false",
+            "MNE_3D_OPTION_SMOOTH_SHADING": "false",
+        }
+    ):
+        yield
+
+
 @pytest.fixture
 @testing.requires_testing_data
-def requires_pyvista():
+def requires_pyvista(options_3d):
     pyvista = pytest.importorskip('pyvista')
     pytest.importorskip('pyvistaqt')
     mne.viz.set_3d_backend('pyvista')
