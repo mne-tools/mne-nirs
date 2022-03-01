ba3f48cd8471d0b6bf2f58b0bb4b3a5259803713
Eric Larson <larson.eric.d@gmail.com>
2022-02-28 21:13:19 -0500

    FIX: Name

--- doc/conf.py
+++ doc/conf.py
@@ -82,7 +82,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MNE-NIRS'
-copyright = u'2020, Robert Luke'
+copyright = u'2022, MNE-NIRS Developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
