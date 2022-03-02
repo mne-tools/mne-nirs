64fa49a7ea08ed3034c26e1babe054977d2e6a48
Eric Larson <larson.eric.d@gmail.com>
2022-03-02 16:00:24 -0500

    FIX: Fix for newly (?) un-skipped examples

--- tools/fold.sh
+++ tools/fold.sh
@@ -5,8 +5,12 @@ cd ~/mne_data/fOLD
 curl -L https://github.com/nirx/fOLD-public/archive/master.zip --output data.zip
 unzip data.zip
 rm data.zip
+FOLD_VAR="MNE_NIRS_FOLD_PATH"
+FOLD_PATH="~/mne_data/fOLD/fOLD-public-master/Supplementary"
 if [ "${CIRCLECI}" == "true" ]; then
-    echo "export MNE_NIRS_FOLD_PATH=~/mne_data/fOLD/fOLD-public-master/Supplementary" >> ${BASH_ENV};
+    echo "export ${FOLD_VAR}=${FOLD_PATH}" >> ${BASH_ENV};
 elif [ "${GITHUB_ACTIONS}" == "true" ]; then
-    echo "MNE_NIRS_FOLD_PATH=~/mne_data/fOLD/fOLD-public-master/Supplementary" >> ${GITHUB_ENV};
+    echo "${FOLD_VAR}=${FOLD_PATH}" >> ${GITHUB_ENV};
+elif [ "${AZURE_CI}" == "true" ] then
+    echo "##vso[task.setvariable variable=${FOLD_VAR}]${FOLD_PATH}"
 fi
