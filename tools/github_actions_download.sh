959ccea725f98eec66bce5aa37029e48ea3437e4
Robert Luke <code@robertluke.net>
2022-05-12 23:19:56 +1000

    Download data

--- tools/github_actions_download.sh
+++ tools/github_actions_download.sh
@@ -2,7 +2,8 @@
 
 if [ "${DEPS}" != "minimal" ]; then
 	python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
-	python -c 'from mne_nirs.datasets import fnirs_motor_group; fnirs_motor_group.data_path(verbose=True)';
-	python -c 'from mne_nirs.datasets import block_speech_noise; block_speech_noise.data_path(verbose=True)';
-	python -c 'from mne_nirs.datasets import audio_or_visual_speech; audio_or_visual_speech.data_path(verbose=True)';
 fi
+
+python -c 'from mne_nirs.datasets import fnirs_motor_group; fnirs_motor_group.data_path(verbose=True)'
+python -c 'from mne_nirs.datasets import block_speech_noise; block_speech_noise.data_path(verbose=True)'
+python -c 'from mne_nirs.datasets import audio_or_visual_speech; audio_or_visual_speech.data_path(verbose=True)'
