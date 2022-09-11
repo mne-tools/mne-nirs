48b6cf15bd52409de86fdcee3510878ac8884c47
Robert Luke <code@robertluke.net>
2022-09-11 11:08:04 +1000

    Debugging file issue

--- tools/github_actions_download.sh
+++ tools/github_actions_download.sh
@@ -7,3 +7,6 @@ fi
 python -c 'from mne_nirs.datasets import fnirs_motor_group; fnirs_motor_group.data_path(force_update=True)'
 python -c 'from mne_nirs.datasets import block_speech_noise; block_speech_noise.data_path(force_update=True)'
 python -c 'from mne_nirs.datasets import audio_or_visual_speech; audio_or_visual_speech.data_path(force_update=True)'
+
+python -c 'from mne.datasets import testing; print(testing.data_path(download=False)'
+ls /home/runner/mne_data
