a8d068d26be54b3164d73378d9285f9d52cf5c57
Robert Luke <code@robertluke.net>
2022-09-17 18:53:41 +1000

    Fix

--- examples/general/plot_14_glm_components.py
+++ examples/general/plot_14_glm_components.py
@@ -210,8 +210,8 @@ for n in [1, 3, 5, 10, 15, 20, 25, 30, 35]:
 
 axes.set_xlabel("Time (s)")
 axes.set_ylabel("Amplitude (AU)")
-plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), cax=axes)
-
+plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=axes)
+plt.show()
 
 # %%
 # Inclusion in Design matrix
