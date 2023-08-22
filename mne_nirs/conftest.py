45b64a5eb19b4ac485302f0e5e7b44ffb5219c1c
Eric Larson <larson.eric.d@gmail.com>
2023-08-22 11:47:56 -0400

    FIX: pandas

--- mne_nirs/conftest.py
+++ mne_nirs/conftest.py
@@ -53,6 +53,8 @@ def pytest_configure(config):
     # seaborn
     ignore:is_categorical_dtype is deprecated.*:FutureWarning
     ignore:use_inf_as_na option is deprecated.*:FutureWarning
+    # nilearn
+    ignore:The provided callable <function sum.*:FutureWarning
     # TODO: in an example (should fix eventually)
     ignore:The behavior of DataFrame concatenation.*:FutureWarning
     """  # noqa: E501
