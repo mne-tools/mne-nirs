44689ce9d0617b8e01cd264238b734ee7b8a1c08
Eric Larson <larson.eric.d@gmail.com>
2023-08-22 11:57:28 -0400

    FIX: Deps

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -2,5 +2,5 @@
 
 
 python -m pip install --upgrade pip setuptools wheel
-python -m pip install --upgrade --progress-bar off -r requirements.txt -r requirements_testing.txt -r requirements_doc.txt git+https://github.com/mne-tools/mne-python.git@main
+python -m pip install --upgrade --progress-bar off -r requirements.txt -r requirements_testing.txt -r requirements_doc.txt git+https://github.com/mne-tools/mne-python.git@main "mne-qt-browser[opengl] @ git+https://github.com/mne-tools/mne-qt-browser.git@main"
 python -m pip install -e .
