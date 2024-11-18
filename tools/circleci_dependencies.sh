8f8c68e10a74f7bd74b0abba91c079388c45f747
Eric Larson <larson.eric.d@gmail.com>
2024-11-18 09:18:53 -0500

    Update tools/circleci_dependencies.sh

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -11,7 +11,7 @@ echo "Installing dependencies"
 python -m pip install --upgrade --progress-bar off \
     -r requirements.txt -r requirements_testing.txt -r requirements_doc.txt \
     "mne-qt-browser[opengl] @ git+https://github.com/mne-tools/mne-qt-browser.git@main" \
-    "mne-python @ git+https://github.com/larsoner/mne-python.git@feature/TD-nirs_snirf" \
+    "https://github.com/larsoner/mne-python/archive/refs/heads/feature/TD-nirs_snirf.zip" \
     "mne-bids @ git+https://github.com/mne-tools/mne-bids.git@main" \
     --only-binary="numpy,scipy,scikit-learn,matplotlib,pandas,ndindex,tables" \
     -e .
