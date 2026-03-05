bd91b7dc960983a21e5fbda9e240ebaefb3a5341
Circle CI <circle@mne.tools>
2026-03-05 21:29:39 +0000

    CircleCI update of dev docs (3275) [ci skip].

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.8.0.dev28+g694acb965',
+    VERSION: '0.8.0.dev29+g0ea15892a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
