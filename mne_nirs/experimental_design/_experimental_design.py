dfc290ad8a99ad25089bcfc9a38207f394319703
Kinkini <kinkinimonaragala@gmail.com>
2026-04-11 17:15:22 -0400

    remove need of statsmodel

--- mne_nirs/experimental_design/_experimental_design.py
+++ mne_nirs/experimental_design/_experimental_design.py
@@ -5,7 +5,7 @@
 import mne
 import numpy as np
 from mne.utils import logger
-from statsmodels.stats.outliers_influence import variance_inflation_factor
+
 
 def make_first_level_design_matrix(
     raw,
@@ -85,6 +85,7 @@ def make_first_level_design_matrix(
     .. footbibliography::
     """
     from nilearn.glm.first_level import make_first_level_design_matrix
+    from nilearn.glm import regression
     from pandas import DataFrame
 
     frame_times = raw.times
@@ -118,14 +119,29 @@ def make_first_level_design_matrix(
     # VIF 1–4 shows low to moderate correlation between predictors 
     # VIF > 4 is often though to indicate high multicollinearity, 
     # which may hint that some varaiables may need to be dropped, combined etc
+    vif_all = []
+    for i in range(dm_no_const.shape[1]):
+        # closely inspired by statsmodels.stats.outliers_influence
+        design_matrix = dm_no_const.values
+        k_vars = (design_matrix).shape[1]
+        design_matrix = np.asarray(design_matrix)
+        x_i = design_matrix[:, i]
+        mask = np.arange(k_vars) != i
+        x_noti = design_matrix[:, mask]
+        
+        #equivaent to r_squared_i = OLS(x_i, x_noti).fit().rsquared
+        #in statsmodels.regression.linear_model
+        regression_out = regression.OLSModel(x_noti).fit(x_i)
+        rss = np.sum(regression_out.residuals ** 2)
+        tss = np.sum((x_i - np.mean(x_i)) ** 2)
+        r_squared_i = 1 - (rss / tss)
 
-    vif = [
-        variance_inflation_factor(dm_no_const.values, i)
-        for i in range(dm_no_const.shape[1])
-    ]
+        vif = 1.0 / (1.0 - r_squared_i)
+        vif_all.append(vif)
+    
 
-    # alert userif high vif detected
-    for name, vif_idx in zip(predictor_names, vif):
+    # alert user if high vif detected
+    for name, vif_idx in zip(predictor_names, vif_all):
         msg = f"{name} with VIF of {vif_idx:.3f}"
         if vif_idx > 4:
             logger.warning("High collinearity " + msg)
